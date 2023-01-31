be3aa0f28f13a51629d5fdc5632e1d84cc2998fb
nanakura <1749094641@qq.com>
2023-01-31 18:14:10 +0800

    fix: range function

--- range.go
+++ range.go
@@ -1,22 +1,22 @@
 package ramda
 
-// TODO fix
-func Range[T Numeric](from T) func(T) []T {
-	return func(to T) []T {
+// Range get an int array in given range
+func Range(from int) func(int) []int {
+	return func(to int) []int {
 		if to >= from {
 			lens := to - from
-			var list []T
-			for i, idx := from, 0; i < lens; idx++ {
+			list := make([]int, 0, lens)
+
+			for i := from; i <= to; i++ {
 				list = append(list, i)
-				i++
 			}
 			return list
 		} else {
 			lens := from - to
-			var list []T
-			for i, idx := to, 0; i < lens; idx++ {
+			list := make([]int, 0, lens)
+
+			for i := from; i >= to; i-- {
 				list = append(list, i)
-				i++
 			}
 			return list
 		}
--- range_test.go
+++ range_test.go
@@ -1,11 +1,15 @@
 package ramda
 
 import (
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestRange(t *testing.T) {
-	// assert.Equal(t, Range[int](1)(3), []int{1, 2, 3})
-	// assert.Equal(t, Range[int](3)(1), []int{3, 2, 1})
-	// assert.Equal(t, Range[int](-3)(-1), []int{-3, -2, -1})
+	assert.Equal(t, Range(1)(3), []int{1, 2, 3})
+	assert.Equal(t, Range(-3)(-1), []int{-3, -2, -1})
+	assert.Equal(t, Range(-3)(3), []int{-3, -2, -1, 0, 1, 2, 3})
+	assert.Equal(t, Range(3)(1), []int{3, 2, 1})
+	assert.Equal(t, Range(-1)(-3), []int{-1, -2, -3})
+	assert.Equal(t, Range(3)(-3), []int{3, 2, 1, 0, -1, -2, -3})
 }
